c57ea555640178f8c384ec2e785ebb1e3009ce4d
Tadeusz Magura-Witkowski <teeed@na1noc.pl>
2014-01-18 13:51:04 +0100

    Removing unused varible from apicompat.py

--- modules/apicompat.py
+++ modules/apicompat.py
@@ -5,9 +5,6 @@ import json
 import ConfigParser
 from application import config
 
-CONFIG_KEY = 'module_apicompat'
-
-
 def update_document(data):
        if not data.get('state'):
            data['state'] = {}
