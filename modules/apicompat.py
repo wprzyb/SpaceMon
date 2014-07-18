ee7c2f2ef1f64624912511fc554d368476443644
Tadeusz Magura-Witkowski <teeed@na1noc.pl>
2014-07-18 20:10:20 +0200

    update_open_lightmask changed to update_open_keys

--- modules/apicompat.py
+++ modules/apicompat.py
@@ -8,5 +8,7 @@ from application import config
 def update_document(data):
        if not data.get('state'):
            data['state'] = {}
+           
        data['open'] = data['state'].get('open', False)
+
        return data
