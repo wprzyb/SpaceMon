0292893a885c298f9a578e88436268d38079f479
Wiktor Przybylski <wikop1@gmail.com>
2015-02-11 05:25:15 +0100

    Added module which gets humidity, pressure, radiation and temperature from HS's graphite

---
+++ modules/graphite.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+
+__version__ = 1.0
+__cacheable__ = True
+
+import urllib2
+import json
+import ConfigParser
+from application import config
+
+CONFIG_KEY = 'module_temp'
+
+
+def update_document(data):
+	remote = urllib2.urlopen("http://graphite.at.hskrk.pl/render/?&target=hs.hardroom.*&format=json&maxDataPoints=1&from=-1min", timeout=2)
+#	remote = urllib2.urlopen(config.get(CONFIG_KEY, 'url'), timeout=config.getint(CONFIG_KEY, 'timeout'))
+	remote = json.load(remote)
+
+	if not data.get('sensors'):
+		data['sensors'] = {}
+	
+	if not data['sensors'].get('temperature'):
+		data['sensors']['temperature'] = []
+	if not data['sensors'].get('humidity'):
+		data['sensors']['humidity'] = []
+	if not data['sensors'].get('radiation'):
+		data['sensors']['radiation'] = {}
+		data['sensors']['radiation']['beta_gamma'] = []
+	if not data['sensors'].get('barometer'):
+		data['sensors']['barometer'] = []
+                                                
+
+	temp = {
+		'value': float(remote[0]['datapoints'][0][0]),
+		'unit': '°C',
+		'location': 'Hardroom',
+	}
+	pressure = {
+		'value': float(remote[1]['datapoints'][0][0]),
+		'unit': 'hPA',
+		'location': 'Hardroom',
+	}
+	hum = {
+		'value': float(remote[2]['datapoints'][0][0]),
+		'unit': '%',
+		'location': 'Hardroom',
+	}
+	rad = {
+		'value': float(remote[3]['datapoints'][0][0]),
+		'unit': 'µSv/h',
+		'location': 'Hardroom',
+	}
+
+	data['sensors']['temperature'].append(temp)
+	data['sensors']['barometer'].append(pressure)
+	data['sensors']['humidity'].append(hum)
+	data['sensors']['radiation']['beta_gamma'].append(rad)
+
+	return data
